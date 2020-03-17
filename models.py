96cf1a63e8d2b2e17777c2d32042723ca1c9dcb0
Mostafa ElAraby <4073898+mostafaelaraby@users.noreply.github.com>
2020-03-17 12:51:06 -0400

    Updated model printing shape

--- models.py
+++ models.py
@@ -52,9 +52,11 @@ class Conv1D(nn.Module):
             x = self.batch_norm(x)
         x = F.leaky_relu(x, negative_slope=self.alpha)
         if self.use_phase_shuffle: 
-            print(x.shape)
+            if self.verbose:
+                print(x.shape)
             x = self.phase_shuffle(x)
-            print(x.shape)
+            if self.verbose:
+                print(x.shape)
         if self.use_drop:
             x = self.dropout(x)
         return x
